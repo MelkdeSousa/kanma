8bd68af8c2cd7d58eb299980eca96ca53a8b56a8
Melk de Sousa <melk.sousap@gmail.com>
2023-03-23 20:46:00 -0300

    :sparkles: add footer to github profile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,8 @@
-import { SidebarButton } from '@/components/SidebarButton'
-import Image from 'next/image'
-import Link from 'next/link'
-import { ReactNode } from 'react'
-import { IoTabletPortraitOutline } from 'react-icons/io5'
+import { SidebarButton } from "@/components/SidebarButton"
+import Image from "next/image"
+import Link from "next/link"
+import { ReactNode } from "react"
+import { IoTabletPortraitOutline } from "react-icons/io5"
 
 export type LayoutProps = {
   children: ReactNode
@@ -10,9 +10,9 @@ export type LayoutProps = {
 
 const sidebarButtons = [
   {
-    title: 'Boards',
+    title: "Boards",
     icon: <IoTabletPortraitOutline />,
-    href: '/boards',
+    href: "/boards",
   },
   // {
   //   title: 'Teams',
@@ -33,25 +33,41 @@ const sidebarButtons = [
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="bg-purple-500 h-screen flex">
-      <aside className="w-fit h-full p-9 flex flex-col items-center">
-        <Link href="/">
+    <div className='bg-purple-500 h-screen flex'>
+      <aside className='w-fit h-full p-9 flex flex-col items-center'>
+        <Link href='/'>
           <Image
-            className="w-14 h-auto cursor-pointer"
-            src="/logo.svg"
-            alt="logo"
+            className='w-14 h-auto cursor-pointer'
+            src='/logo.svg'
+            alt='logo'
             width={300}
             height={300}
           />
         </Link>
 
-        <nav className="flex flex-1 flex-col mt-9 gap-6">
+        <nav className='flex flex-1 flex-col mt-9 gap-6'>
           {sidebarButtons.map((button) => (
             <SidebarButton key={button.title} {...button} />
           ))}
         </nav>
+
+        <Link href='https://github.com/melkdesousa' target='_blank'>
+          <footer className='flex flex-col items-center mt-9 gap-4'>
+            <Image
+              className='w-14 h-auto cursor-pointer rounded-full'
+              src='https://github.com/melkdesousa.png'
+              alt='profile picture of melk de sousa'
+              width={100}
+              height={100}
+            />
+
+            <span className='text-xs text-white-500'>
+              &copy; {new Date().getFullYear()} Melk de Sousa
+            </span>
+          </footer>
+        </Link>
       </aside>
-      <main className="bg-white-500 w-full h-auto mt-1 rounded-tl-3xl">
+      <main className='bg-white-500 w-full h-auto mt-1 rounded-tl-3xl'>
         {children}
       </main>
     </div>
